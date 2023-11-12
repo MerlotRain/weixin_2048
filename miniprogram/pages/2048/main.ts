0477119e615f39ece84c027d1a7d8548987092e9
Merlot <1198477036@qq.com>
2023-11-12 08:43:00 +0800

    sync

--- miniprogram/pages/2048/main.ts
+++ miniprogram/pages/2048/main.ts
@@ -1,42 +1,133 @@
 /// <reference path="./grid.ts" />
 
 class Main {
-	private size:number;
-	private startData:number;
-	private board:Board;
+  private size: number;
+  private startData: number;
+  private board: Board;
 
-	constructor(size:number) {
-		this.size = size;
-		this.startData = 2;
-		this.board = new Board(size);
-		this.addRandomData();
-		this.startData = 1;
-	}
+  constructor(size: number) {
+    this.size = size;
+    this.startData = 2;
+    this.board = new Board(size);
+    this.setRandomData();
+    this.startData = 1;
+  }
 
-	addRandomData() {
-		if(!this.board.cellEmpty()) {
-			let value = Math.random() < 0.9 ? 2 : 4;
-			let cell = this.board.selectedCell() as Cell;
-			cell.value = value;
-			this.update(cell);
-		}
-	}
+  setRandomData() {
+    for (let i = 0; i < this.startData; i++) {
+      this.addRandomData();
+    }
+  }
 
-	update(cell:Cell) {
-		this.board.grid[cell.x][cell.y] = cell.value;
-	}
+  addRandomData() {
+    if (!this.board.cellEmpty()) {
+      let value = Math.random() < 0.9 ? 2 : 4;
+      let cell = this.board.selectedCell() as Cell;
+      cell.value = value;
+      this.update(cell);
+    }
+  }
 
-	move(dir: number) {
-	}
+  update(cell: Cell) {
+    this.board.grid[cell.x][cell.y] = cell.value;
+  }
 
-	formList(dir: number) {
-		for(let i=0;i<this.size;i++) {
-			for(let j=0; j<this.size;j++) {
-				switch(dir) {
-					case 0:
-				}
+  move(dir: number): number[][] {
+    let curList = this.formList(dir);
 
-			}
-		}
-	}
+    let list = this.combine(curList);
+    let result: number[][] = new Array(this.size);
+    for (let i = 0; i < this.size; i++) {
+      result[i] = new Array(this.size);
+      for (let j = 0; j < this.size; j++) {
+        switch (dir) {
+          case 0:
+            result[i][j] = list[j][i];
+            break;
+          case 1:
+            result[i][j] = list[i][this.size - 1 - j];
+            break;
+          case 2:
+            result[i][j] = list[j][this.size - 1 - i];
+            break;
+          case 3:
+            result[i][j] = list[i][j];
+            break;
+        }
+      }
+    }
+    this.board.grid = result;
+    this.setRandomData();
+    return result;
+  }
+
+  formList(dir: number) {
+    let list: number[][] = new Array(this.size);
+    for (let i = 0; i < this.size; i++) {
+      list[i] = new Array(this.size);
+      for (let j = 0; j < this.size; j++) {
+        switch (dir) {
+          case 0:
+            list[i].push(this.board.grid[j][i]);
+            break;
+          case 1:
+            list[i].push(this.board.grid[i][this.size - 1 - j]);
+            break;
+          case 2:
+            list[i].push(this.board.grid[this.size - 1 - j][i]);
+            break;
+          case 3:
+            list[i].push(this.board.grid[i][j]);
+            break;
+        }
+      }
+    }
+    return list;
+  }
+
+  combine(list: number[][]): number[][] {
+    for (let i = 0; i < list.length; i++) {
+      list[i] = this.changeItem(list[i]);
+    }
+
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 1; j < this.size; j++) {
+        if (list[i][j - 1] == list[i][j] && list[i][j] != -1) {
+          list[i][j - 1] += list[i][j];
+          list[i][j] = -1;
+        }
+      }
+    }
+
+    for (let i = 0; i < list.length; i++) list[i] = this.changeItem(list[i]);
+
+    return list;
+  }
+
+  changeItem(item: number[]): number[] {
+    let cnt = 0;
+    for (let i = 0; i < item.length; i++) {
+      if (item[i] != -1) item[cnt++] = item[i];
+    }
+    for (let i = cnt; i < item.length; i++) item[i] = -1;
+    return item;
+  }
+
+  isOver() {
+    if (!this.board.cellEmpty()) {
+      return false;
+    } else {
+      for (let i = 0; i < this.size; i++) {
+        for (let j = 0; j < this.size; j++) {
+          if (this.board.grid[i][j] == this.board.grid[i][j - 1]) return false;
+        }
+      }
+      for (let i = 0; i < this.size; i++) {
+        for (let j = 0; j < this.size; j++) {
+          if (this.board.grid[i][j] == this.board.grid[i - 1][j]) return false;
+        }
+      }
+    }
+    return true;
+  }
 }
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,31 +1,31 @@
 // index.ts
 // 获取应用实例
-const app = getApp<IAppOption>()
+const app = getApp<IAppOption>();
 
 Page({
   data: {
-    current: 0
-  },  
-  onReady: function() {
+    current: 0,
+  },
+  onReady: function () {
     this.load();
   },
-  load: function() {
+  load: function () {
     var n = 1;
-    var timer = setInterval(()=>{
-      if(n == 6) {
+    var timer = setInterval(() => {
+      if (n == 6) {
         clearInterval(timer);
         // wx.redirectTo({
         //   url: '../2048/2048'
         // })
       }
       this.setData({
-        current: this.data.current+1
+        current: this.data.current + 1,
       });
-      if(this.data.current > 3) 
+      if (this.data.current > 3)
         this.setData({
-          current: 0
+          current: 0,
         });
-        n++;
+      n++;
     }, 400);
-  }
-})
+  },
+});
